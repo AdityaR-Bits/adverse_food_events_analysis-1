d901603f96d47e77cfc8f012c305211527109023
Rajasvi <rajasvivinayak@gmail.com>
2021-11-07 19:08:37 -0800

    first commit ece143

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Detailed EDA of adverse food events reported to FDA.',
+    author='Rajasvi',
+    license='MIT',
+)
