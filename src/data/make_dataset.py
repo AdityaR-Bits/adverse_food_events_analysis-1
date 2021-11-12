1d94cc07d4424b926b32552994bf9d7e80cb2cdb
JWRickyWan <jianghongwan1996@gmail.com>
2021-11-11 17:24:54 -0800

    adding data directory

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -44,5 +44,4 @@ def main(
 if __name__ == "__main__":
     log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
     main()
