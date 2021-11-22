d6ce118fdfa8d9dcfb00cd6b245e45923052b425
Rajasvi <rajasvivinayak@gmail.com>
2021-11-21 17:20:32 -0800

    product_outcome.ipynb added

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,6 +3,27 @@ import click
 import logging
 from pathlib import Path
 import pandas as pd
+import re
+import string
+from nltk.corpus import stopwords
+
+
+def brand_preprocess(row, trim_len=2):
+
+    regex = re.compile("[%s]" % re.escape(string.punctuation))
+    x = regex.sub("", row["product"])
+
+    stop = stopwords.words("english")
+    n_x = [_.upper() for _ in x.lower().split(" ") if _ not in stop]
+
+    if len(n_x) == 0:
+        return ""
+    if row["category"] in [
+        "Nuts/Edible Seed",
+        "Vit/Min/Prot/Unconv Diet(Human/Animal)",
+    ]:
+        return " ".join(n_x) if len(n_x) < trim_len else " ".join(n_x[:trim_len])
+    return n_x[0]
 
 
 @click.command()
@@ -15,19 +36,19 @@ def main(
         cleaned data ready to be analyzed (saved in ../processed).
     """
     outPath = Path(output_dirpath)
+    inPath = Path(input_dirpath)
 
     logger = logging.getLogger(__name__)
     logger.info("making final data set from raw data")
-    raw_dir = Path(input_dirpath)
 
     aggReports = None
 
-    def clean_data(x):
+    def strip_str(x):
         if isinstance(x, str):
             x = x.strip()
         return x
 
-    for p in list(raw_dir.glob("*.csv")):
+    for p in list(inPath.glob("*.csv")):
 
         curr_df = pd.read_csv(p, encoding="unicode_escape")
 
@@ -36,7 +57,7 @@ def main(
         curr_df = curr_df.rename(
             columns={"meddra_preferred_terms": "medra_preferred_terms"}
         )
-        curr_df = curr_df.applymap(clean_data)
+        curr_df = curr_df.applymap(strip_str)
 
         aggReports = curr_df if aggReports is None else pd.concat([aggReports, curr_df])
 
@@ -44,6 +65,8 @@ def main(
     aggReports.reset_index(drop=True, inplace=True)
 
     aggReports.to_csv(outPath / "clean_data.csv")
+
+    logger.info("making outcomes exploded data set from clean data")
 
     # Create exploded outcome-wise cleaned data
     aggReports.outcomes = aggReports.outcomes.apply(
@@ -55,6 +78,12 @@ def main(
     expl_aggReports = expl_aggReports.reset_index(drop=True)
 
     expl_aggReports.to_csv(outPath / "exploded_out.csv")
+
+    logger.info("making data with clean product column from processed data")
+    # Create brand-enriched clean data.
+    aggReports = aggReports[aggReports["product"].notna()]
+    aggReports.brand = aggReports.apply(brand_preprocess, axis=1)
+    aggReports.to_csv(outPath / "clean_brand_data.csv")
 
 
 if __name__ == "__main__":
