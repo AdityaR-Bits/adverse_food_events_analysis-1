7f24e12e2b7973642d2d9ddc59e5dffacc063fc5
Rajasvi <rajasvivinayak@gmail.com>
2021-12-01 16:57:03 -0800

    put docstrings for visualize.py

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,8 +1,9 @@
 import plotly.express as px
+import pandas as pd
 
 
-def brands_vs_outcomes_plot(
-    base_df,
+def brandsVsOutcomesPlot(
+    baseDf,
     category,
     title,
     relv_outcomes=[
@@ -13,29 +14,41 @@ def brands_vs_outcomes_plot(
         "Patient Visited ER",
     ],
 ):
-    """[summary]
+    """ This function plots histogram for the brand names for each category colored with respect to all outcomes.
 
     Args:
-        base_df ([type]): [description]
-        category ([type]): [description]
-        title ([type]): [description]
-        relv_outcomes (list, optional): [description]. Defaults to [ "Death", "Life Threatening", "Hospitalization", "Disability", "Patient Visited ER", ].
+        base_df (pd.DataFrame): Base dataframe with all data.
+        category (str): Category for which brands have to be plotted.
+        title (str): Title of plot
+        relv_outcomes (list, optional): Relevant serious outcomes which are considered. Defaults to [ "Death", "Life Threatening", "Hospitalization", "Disability", "Patient Visited ER", ].
     """
 
-    df = base_df[
-        (base_df["category"] == category) & (base_df["product"] != "EXEMPTION 4")
+    assert isinstance(
+        baseDf, pd.DataFrame
+    ), "Check whether baseDf is Pandas Dataframe or not."
+    assert isinstance(category, str), "Check whether category is string or not."
+    assert isinstance(title, str), "Check whether title is string or not."
+    assert isinstance(
+        relv_outcomes, list
+    ), "Check whether relv_outcomes is list or not."
+    assert len(relv_outcomes) > 1, "Atleast 1 relevant outcome must be selected"
+
+    df = baseDf[
+        (baseDf["category"] == category) & (baseDf["product"] != "EXEMPTION 4")
     ].copy()
     df.dropna(inplace=True)
 
-    g = df.groupby(["brand"])["report_id"].count().sort_values(ascending=False)
+    topBrandsGroup = (
+        df.groupby(["brand"])["report_id"].count().sort_values(ascending=False)
+    )
 
-    relv_brands = list(g.reset_index()["brand"].values[:10])
+    relv_brands = list(topBrandsGroup.reset_index()["brand"].values[:10])
     relv_df = df[df["outcomes"].isin(relv_outcomes)]
     relv_df = relv_df[relv_df["brand"].isin(relv_brands)]
 
     relv_df = relv_df.rename(columns={"outcomes": "Outcomes"})
 
-    plot_bar_histogram(relv_df, title=title, x="brand", color="Outcomes")
+    plotBarHistogram(relv_df, title=title, x="brand", color="Outcomes")
 
     df = df[df["outcomes"].isin(relv_outcomes)]
     g_top = df.groupby(["brand"])["report_id"].count().sort_values(ascending=False)
@@ -53,9 +66,26 @@ def brands_vs_outcomes_plot(
     fig_pie.show()
 
 
-def plot_bar_histogram(
+def plotBarHistogram(
     df, title, x="brand", color="Outcomes", barmode="stack", logscale=False
 ):
+    """This function plots bar histogram for columnn in dataframe with color as another column.
+
+    Args:
+        df (pd.DataFrame): input pandas dataframe
+        title (str): title of bar plot
+        x (str, optional): x-axis column name. Defaults to "brand".
+        color (str, optional): column name for color. Defaults to "Outcomes".
+        barmode (str, optional): bar mode-stack or group . Defaults to "stack".
+        logscale (bool, optional): whether y-axis (count) has to be log-scaled or not. Defaults to False.
+    """
+
+    assert isinstance(df, pd.DataFrame), "Check whether df is Pandas Dataframe or not."
+    assert isinstance(color, str), "Check whether color is string or not."
+    assert isinstance(title, str), "Check whether title is string or not."
+    assert isinstance(barmode, str), "Check whether title is string or not."
+    assert isinstance(logscale, bool), "Check whether logscale is bool or not."
+
     fig = px.histogram(
         df,
         x=x,
@@ -72,7 +102,6 @@ def plot_bar_histogram(
         legend=dict(font=dict(family="Arial", size=20, color="black")),
         legend_title=dict(font=dict(family="Arial", size=20, color="#424242")),
     )
-
     fig.update_layout(
         xaxis_title="Top 10 Brands",
         yaxis_title="Number  of  Adverse  Events",
