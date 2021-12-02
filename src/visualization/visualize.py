334c119baf1cfecb42f5d155a8a0ccad09dbb29f
JWRickyWan <jianghongwan1996@gmail.com>
2021-12-01 17:32:26 -0800

    working visualize for time plots

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -87,7 +87,7 @@ def plot_bar_histogram(
     fig.show()
 
 
-def plot_time_trend(df, title, x_col="date", y_col="count"):
+def plot_time_trend(df, title, x_col="date", y_col="counts"):
     """
 
     Args:
@@ -106,7 +106,8 @@ def plot_time_trend(df, title, x_col="date", y_col="count"):
     year_month = pd.DataFrame(df['time_stamp'].groupby(df.time_stamp.dt.to_period("M")).agg('count').items(),
                               columns=["date", "counts"]).sort_values(by=["date"])
     year_month["date"] = year_month["date"].apply(lambda x: x.to_timestamp())
-    px.line(year_month, x=x_col, y=y_col, title=title)
+    fig = px.line(year_month, x=x_col, y=y_col, title=title)
+    fig.show()
 
 
 def plot_pie_subplots_yearly(group, title, column_name, dropping=False, d_threshold=1 / 50):
@@ -154,7 +155,7 @@ def plot_pie_subplots_yearly(group, title, column_name, dropping=False, d_thresh
     fig.show()
 
 
-def plot_scatters(group, group_names, title, filter=False, filter_list=None, file_path=None):
+def plot_scatters(group, group_names, title, fil=False, filter_list=None, plot_now=False):
     """
 
     Args:
@@ -170,13 +171,13 @@ def plot_scatters(group, group_names, title, filter=False, filter_list=None, fil
     """
     assert isinstance(group, pd.core.groupby.generic.DataFrameGroupBy)
     assert isinstance(group_names, list) and all(isinstance(x, str) for x in group_names)
-    assert isinstance(filter, bool)
-    assert isinstance(filter_list, list) or filter is None
-    assert isinstance(file_path, str) or file_path is None
+    assert isinstance(fil, bool)
+    assert isinstance(filter_list, list) or filter_list is None
+    assert isinstance(plot_now, bool)
     assert isinstance(title, str)
     fig = go.Figure()
     for i in range(len(group_names)):
-        if filter and group_names[i] in filter_list:
+        if fil and group_names[i] in filter_list:
             continue
         outcome = group.get_group(group_names[i])
         outcome_df = pd.DataFrame(outcome["time_stamp"].groupby(outcome.time_stamp.dt.to_period("M")).
@@ -190,16 +191,16 @@ def plot_scatters(group, group_names, title, filter=False, filter_list=None, fil
         ))
     fig.update_traces(hoverinfo='text+name', mode='lines')
     fig.update_layout(title_text=title)
-    fig.update_layout(legend=dict(font=dict(family="Times", size=25, color="black")))
+    fig.update_layout(legend=dict(font=dict(family="Times", size=15, color="black")))
     fig.update_layout(
         xaxis_title="Date",
         yaxis_title="Count",
         font=dict(
             family="Times",
-            size=25,
+            size=15,
             color="#7f7f7f"
         ))
-    if file_path is None:
+    if plot_now:
         fig.show()
     else:
         return fig
@@ -222,7 +223,7 @@ def get_quorn_pie(exploded_df):
     count = list(outcome_quorn["outcomes"].agg("count").sort_values(ascending=False))
     outcomes = list(outcome_quorn["outcomes"].agg("count").sort_values(ascending=False).index)
     fig.add_trace(go.Pie(values=count, labels=outcomes, textinfo='none', title="outcome from QUORN"))
-
+    fig.show()
 
 def get_quorn_bar(exploded_df):
     """
@@ -236,9 +237,45 @@ def get_quorn_bar(exploded_df):
     assert isinstance(exploded_df, pd.DataFrame)
     quorn = exploded_df[exploded_df.products.str.contains("QUORN") == True]
     k = quorn[["outcomes", "year"]]
-    px.histogram(
+    fig = px.histogram(
         k,
         x="year",
         color="outcomes",
         title='Outcome histogram for Quorn'
     )
+    fig.show()
+
+def plot_normalized_scatters(groups,group_names):
+    """
+
+    Args:
+        df: input dataframe
+        groups: groups that want to plot and normalized
+
+    Returns:a normalized scatter over time
+
+    """
+    assert isinstance(groups, list)
+    assert all(isinstance(x,pd.DataFrame) for x in groups)
+    assert isinstance(group_names, list) and all(isinstance(x, str) for x in group_names)
+    fig = go.Figure()
+    for i in range(len(group_names)):
+        cat = groups[i]
+        max_num = max(cat["time_stamp"].groupby(cat.time_stamp.dt.to_period("M")).agg('count'))
+        category_df = pd.DataFrame((cat["time_stamp"].groupby(cat.time_stamp.dt.to_period("M")).
+                                    agg('count') / max_num).items(), columns=["date", "counts"]).sort_values(
+            by=["date"])
+        category_df["date"] = category_df["date"].apply(lambda x: x.to_timestamp())
+        fig.add_trace(go.Scatter(x=category_df["date"], y=category_df["counts"], mode="lines", name=group_names[i]))
+    fig.update_traces(hoverinfo='text+name', mode='lines')
+    fig.update_layout(title_text="Normalized categories over time")
+    fig.update_layout(legend=dict(font=dict(family="Times", size=15, color="black")))
+    fig.update_layout(
+        xaxis_title="Date",
+        font=dict(
+            family="Times",
+            size=15,
+            color="#7f7f7f"
+        ))
+    fig.show()
+
